41bb9f9dd5d51142cb230696934a116885e9ed50
Minghe <605982123@qq.com>
2022-11-29 23:05:03 +0800

    third commit

--- src/main/java/com/jmh/git/GitTest.java
+++ src/main/java/com/jmh/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello jmh!");
+        System.out.println("hello lance!");
     }
 }
