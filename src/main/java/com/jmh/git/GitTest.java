83d9b64a84bbe2e30dfac9556c9a3178c5b55bb2
Minghe <605982123@qq.com>
2022-12-01 23:39:36 +0800

    hot-fix commit-1

--- src/main/java/com/jmh/git/GitTest.java
+++ src/main/java/com/jmh/git/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("hello lance");
         System.out.println("hot-fix");
         System.out.println("master");
+        System.out.println("push test");
     }
 }
